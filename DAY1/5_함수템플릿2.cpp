1c23f10b83c8a013332e78c90371216192d43e82
ecourse <smkang@codenuri.co.kr>
2025-09-22 14:13:02 +0900

    Add files via upload

---
+++ DAY1/5_함수템플릿2.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+template<typename T>
+T Add(T a, T b)
+{
+	return a + b;
+}
+
+int main()
+{
+	// #1. 아래 코드는 왜 에러일까요 ? 에러의 정확인 원인 ?
+	std::cout << Add(1, 2.1) << std::endl;
+	
+}
+
