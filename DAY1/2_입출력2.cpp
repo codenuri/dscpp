b584c73d290ca361048c4177dfe99678b3de316c
ecourse <smkang@codenuri.co.kr>
2025-09-22 10:31:06 +0900

    Add files via upload

--- DAY1/2_입출력2.cpp
+++ DAY1/2_입출력2.cpp
@@ -21,10 +21,13 @@ int main()
 	double d = 1.0123456789;
 
 	std::cout << d << std::endl; // 몇자리까지 출력되는지 확인하세요
+									// => 6자리에서 반올림,5자리까지 출력
 
 	// d 를 10자리 까지 출력해 보세요
 	// #1. cppreference.com 에서 std::setw() 찾으세요
 	// #2. 상위 문서로 이동.
 	// #3. 모든 조정자 목록에서 원하는 것 찾으세요
 	// #4. 예제를 보고 사용법을 익히세요. 
+
+	std::cout << std::setprecision(10) << d << std::endl;
 }
