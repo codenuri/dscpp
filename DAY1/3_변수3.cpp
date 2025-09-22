56858f5f1ad82825fa1e4d553983ad385294fe6a
ecourse <smkang@codenuri.co.kr>
2025-09-22 11:17:08 +0900

    Add files via upload

---
+++ DAY1/3_변수3.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+struct Point
+{
+	int x, y;
+};
+
+void foo(int n)    {} 
+
+void goo(Point pt) {}
+
+int main()
+{
+	Point p = { 1,2 };
+
+	foo(3);	    // int n = 3
+	foo({ 3 });	// int n = {3}
+
+	goo(p);		// Point pt = p
+	goo({1,1});	// Point pt = {1,1}
+				// 요즘 널리 사용되는 표기법. 알아 두세요
+				// C+11 의 {} 초기화 때문에 가능해진 문법
+}
+Point hoo()
+{
+	return { 0, 0 }; // 이 표현도 가능
+					 // return Point{0,0} 의 의미. 
+}
+
+
+
+
