693ad9457d57519919c67a79e0e96b04e5148457
ecourse <smkang@codenuri.co.kr>
2025-09-22 09:43:41 +0900

    Add files via upload

---
+++ DAY1/1_namespace3.cpp
@@ -0,0 +1,16 @@
+﻿// 1_namespace3.cpp - 9page 내용
+// 
+#include <algorithm>
+
+using namespace std; // 모든 C++ 표준을 std 이름 없이 사용
+
+// #1. C++ 표준의 모든 함수는 std 이름 공간 안에 있습니다.
+
+int count = 0;
+int main()
+{
+	int n = min(3, 2); // C++ 표준 함수. algorithm 헤더 필요
+//	int n = std::min(3, 2);
+
+	int k = count; // 이 코드가 왜 에러일까요 ??
+}
