593bacd72ee30ecea18dd20444ec94735fcb15d9
ecourse <smkang@codenuri.co.kr>
2025-09-22 15:46:23 +0900

    Add files via upload

---
+++ DAY1/8_캐스팅2.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+// C++은 용도별로 사용하기 위해
+// => 4개의 캐스팅 연산자를 제공합니다.
+// => 반드시 모든 C++ 코드는 아래 4개만을 사용해서 캐스팅해야 합니다.
+// => static_cast
+// => reinterpret_cast
+// => const_cast
+// => dynamic_cast
+
+
+// static_cast : 논리적으로 맞고 위험하지 않은 경우만 허용
+//          void*=> 다른 타입*,
+//			상속관계의 캐스팅 등.
+//          반드시 연관성이 있어야 한다
+
+
+int main()
+{
+	double d = 3.4;
+	int    n = d;
+
+
+//	int* p1 = malloc(100);     // C언어 : ok. C++ : 에러
+
+
+}
+
