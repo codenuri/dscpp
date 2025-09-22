55aa6731163cd9e7feaea2aaa99f81e60d46730d
ecourse <smkang@codenuri.co.kr>
2025-09-22 15:09:09 +0900

    Add files via upload

--- DAY1/6_반복문2.cpp
+++ DAY1/6_반복문2.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <range> // C++20 에서 추가된 헤더
+#include <ranges> // C++20 에서 추가된 헤더
 
 // C++언어 버전 : C++20 이상 으로 해야 합니다.
 int main()
@@ -7,7 +7,7 @@ int main()
 	int x[10] = { 1,2,3,4,5,6,7,8,9,10 };
 
 //	for (auto e : x)	// 전체 열거
-	for (auto e : std::view::reverse(x) )	// 거꾸로 열거
+	for (auto e : std::views::reverse(x) )	// 거꾸로 열거
 	{
 		std::cout << e << std::endl;
 	}
