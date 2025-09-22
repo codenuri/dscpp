f6ec4acb6affe1327bd01111cb42466e0a804053
ecourse <smkang@codenuri.co.kr>
2025-09-22 14:47:53 +0900

    Add files via upload

--- DAY1/5_함수템플릿7.cpp
+++ DAY1/5_함수템플릿7.cpp
@@ -1,14 +1,19 @@
 // 함수 템플릿 모양 총정리
 // 1. 아래 코드가 C++98 시절 부터 사용되던 모양
 // 2. typename 대신 class 키워드 가능
-// 
+/*
 //template<typename T>
 template<class T>
 T square(T a)
 {
 	return a * a;
 }
-
+*/
+// C++20 부터는 위코드 만들때 아래 처럼
+auto square(auto a)
+{
+	return a * a;
+}
 int main()
 {
 	square(3);		
