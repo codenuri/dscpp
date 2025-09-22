0bbd2e7329df4239454ca9606d56162c96f29f8e
ecourse <smkang@codenuri.co.kr>
2025-09-22 11:03:15 +0900

    Add files via upload

---
+++ DAY1/3_변수2.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+// 26page - 중요한 변화.!
+
+struct Point
+{
+	int x, y;
+};
+int main()
+{
+	// C/C++98 언어의 초기화의 단점
+	// => 변수의 종류에 따라 초기화 방법이 다르다.
+	int n1 = 10;
+	int x1[3] = { 1,2,3 };
+	Point p1 = { 1,2 };
+
+	// C++11 "uniform initialization" 이라는 문법
+	// => 모든 종류의 변수는 동일한 방법으로 초기화 할수 있다
+	int n2 = { 10 };
+	int x2[3] = { 1,2,3 };
+	Point p2 = { 1,2 };
+
+}
+
+
+
