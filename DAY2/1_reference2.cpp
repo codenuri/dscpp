c775e16aed48a566e6d59bec6b5e2184d9c0bf19
ecourse <smkang@codenuri.co.kr>
2025-09-23 08:47:37 +0900

    Add files via upload

---
+++ DAY2/1_reference2.cpp
@@ -0,0 +1,20 @@
+﻿// 1_레퍼런스2
+#include <iostream>
+// 61 page - 그림 꼭 알아 두세요
+
+void inc1(int n)  { ++n; }
+void inc2(int* p) { ++(*p); }
+void inc3(int& r) { ++r; }
+
+int main()
+{
+	int a = 1, b = 1, c = 1;
+
+	inc1( a ); // call by value, a 증가 실패
+	inc2( &b); // call by address, b 증가 성공
+	inc3( c ); // call by reference, c 증가 성공
+
+	std::cout << a << std::endl; // 1
+	std::cout << b << std::endl; // 2
+	std::cout << c << std::endl; // 2
+}
