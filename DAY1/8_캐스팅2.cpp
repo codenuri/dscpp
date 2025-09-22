5bf93e096f314b41d9c3d4a7a79133a020620638
ecourse <smkang@codenuri.co.kr>
2025-09-22 15:48:29 +0900

    Add files via upload

--- DAY1/8_캐스팅2.cpp
+++ DAY1/8_캐스팅2.cpp
@@ -9,7 +9,9 @@
 // => dynamic_cast
 
 
-// static_cast : 논리적으로 맞고 위험하지 않은 경우만 허용
+// static_cast : 기본 캐스팅
+//		    논리적으로 맞고 위험하지 않은 경우만 허용
+//		    기본 타입의 값 캐스팅
 //          void*=> 다른 타입*,
 //			상속관계의 캐스팅 등.
 //          반드시 연관성이 있어야 한다
@@ -18,7 +20,8 @@
 int main()
 {
 	double d = 3.4;
-	int    n = d;
+//	int    n = d;	// ok. 단, 경고 발생할수 있다.
+	int    n = static_cast<int>(d); // ok. 의도를 표현, 경고 없음
 
 
 //	int* p1 = malloc(100);     // C언어 : ok. C++ : 에러
