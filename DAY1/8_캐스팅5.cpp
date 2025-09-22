3c7457b2b13c088b7764424b16abdf636fbfafbc
ecourse <smkang@codenuri.co.kr>
2025-09-22 16:27:54 +0900

    Add files via upload

--- DAY1/8_캐스팅5.cpp
+++ DAY1/8_캐스팅5.cpp
@@ -6,7 +6,14 @@
 	double* p1 = (double*)&c; // ok.  C 스타일은 대부분 허용
 
 	// 위 코드를 C++ 스타일로 변경해 보세요
-	double* p2 = ? ;
+
+//	double* p2 = reinterpret_cast<double*>(&c); // error. const 제거 못함
+	
+//	double* p2 = const_cast<double*>(&c); // error. 동일 타입의 const 제거만 가능
+
+	double* p2 = reinterpret_cast<double*>(const_cast<int*>(&c));
+					// const int* => int*
+					// int* => double*
 }
 
 
