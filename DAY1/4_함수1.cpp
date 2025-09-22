3afe1bc9d983e2b5fb234bd962aa41b92b13316f
ecourse <smkang@codenuri.co.kr>
2025-09-22 13:09:26 +0900

    Add files via upload

--- DAY1/4_함수1.cpp
+++ DAY1/4_함수1.cpp
@@ -6,8 +6,11 @@
 
 // 주의 #2. 마지막 인자부터 차례대로만 표기 가능
 
-void f1(int a,     int b = 0, int c)     {}
-void f2(int a = 0, int b,     int c = 0) {}
+// 원리 : 호출하는 코드에 디폴트 값을 채우는것
+// => godbolt.org 에서 어셈블리코드 확인
+
+void f1(int a,     int b = 0, int c)     {} // error
+void f2(int a = 0, int b,     int c = 0) {} // error
 
 void foo(int a = 0, int b = 0, int c = 0);
 
@@ -15,7 +18,7 @@ int main()
 {
 	foo(1, 2, 3);
 	foo(1, 2);
-	foo(1);
+	foo(1);		// foo(1, 0, 0)
 	foo();		// foo(0, 0, 0)
 }
 
