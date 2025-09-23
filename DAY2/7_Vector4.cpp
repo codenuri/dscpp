14bd16d9606471bf770dfc7751fc9c5052610978
ecourse <smkang@codenuri.co.kr>
2025-09-23 13:31:56 +0900

    Add files via upload

--- DAY2/7_Vector4.cpp
+++ DAY2/7_Vector4.cpp
@@ -4,6 +4,7 @@
 // Vector3.cpp 처럼 크기를 변경되는 배열을 "동적 배열" 또는 "Vector" 라고 합니다.
 
 // 객체지향 : 필요한 타입을 먼저 설계 하자.!!
+// => Rect, Point, People 뿐 아니라.. Vector, List, Tree 등 다양한 자료구조도 타입으로
 // => Vector 타입을 설계해 봅시다.
 
 class Vector
