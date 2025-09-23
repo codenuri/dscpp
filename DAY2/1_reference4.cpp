9411591da3e815f2efd8cd63d60615dcf2f9af6b
ecourse <smkang@codenuri.co.kr>
2025-09-23 09:17:46 +0900

    Add files via upload

--- DAY2/1_reference4.cpp
+++ DAY2/1_reference4.cpp
@@ -20,3 +20,22 @@ int main()
 	f1(x);
 	f2(x);
 }
+
+// 정리
+// in parameter : 전달 받은 인자를 읽기만 하는 파라미터
+// out parameter : 전달 받은 인자를 변경하거나, 값을 담아주는 것(inc 함수)
+
+// 1. out parameter 만들때
+// pointer 또는 reference 모두 가능
+// => 성능 차이는 거의 없음
+// => 단, 안전성을 고려하면 reference 가 좋음.
+void inc1(int* p) { ++(*p); } // good
+void inc2(int& r) { ++r; } // best
+
+// 2. in parameter
+// => 인자가 primitive type(int, double) : call by value 가 좋다
+// => ex) void f1(int n) {}
+
+// => 인자가 user define type 이고 크기가 크다면 (보통 8바이트 이상의크기)
+// => const & 가 좋다.
+// => ex) void foo(const Rect& rc) {}
