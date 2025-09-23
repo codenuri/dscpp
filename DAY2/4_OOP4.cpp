25342ee67d3d43ec59b68e2cce34cb3994786307
ecourse <smkang@codenuri.co.kr>
2025-09-23 10:29:03 +0900

    Add files via upload

---
+++ DAY2/4_OOP4.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+struct Rect
+{
+	int left;
+	int top;
+	int right;
+	int bottom;
+
+	int getArea() { return (right - left) * (bottom - top); }
+	void draw() { std::cout << "draw rect" << std::endl; }
+};
+int main()
+{
+	// #1. 용어 
+	int n = 10;		// n 은 "변 하는 수"
+	n = 20;			// 그래서 "변수" 라고 합니다.
+
+	Rect r = { 1,1,10,10 };
+	// r 은 변하는 수 보다는 "사각형 한개"를 표현
+	// => 객체라고 합니다.
+
+	// 객체 : 세상에 존재하는 모든 것들.
+	//        r은 메모리에 존재하는 사각형에 대한 표현
+	// n 을 변수가 아닌 객체라고 해도 됩니다.
+
+	// 그런데, 관례상
+	// primitive        를 메모리에 생성한것 : 변수
+	// user define type 를 메모리에 생성한것 : 객체
+
+
+	// #2. 메모리 layout
+	Rect r1 = { 1,1,10,10 }; 
+	Rect r2 = { 1,1,10,10 };
+}
