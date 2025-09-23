919efe734242cdb331f80be47d97a28c29753f18
ecourse <smkang@codenuri.co.kr>
2025-09-23 11:24:10 +0900

    Add files via upload

--- DAY2/6_생성자2.cpp
+++ DAY2/6_생성자2.cpp
@@ -16,7 +16,6 @@ public:
 	{
 		name = n;
 		age = a;
-
 		std::cout << "생성자1\n";
 	}
 	Person()
@@ -25,6 +24,12 @@ public:
 		age = 0;
 		std::cout << "생성자2\n";
 	}
+	// 소멸자(destructor)
+	// => ~클래스이름()
+	// => 오직 한개만 만들수 있다.
+	// => 반환타입이 없고, 인자도 없다.
+	// => 객체가 파괴 될때 호출
+	~Person() { std::cout << "Person 파괴\n"; }
 };
 
 int main()
@@ -32,7 +37,7 @@ int main()
 	Person p1("kim", 28); // 생성자 1사용
 	Person p2;			  // 생성자 2사용
 
-}
+} //<== p1, p2 파괴. 소멸자 호출됨. 
 
 
 
