78f5750828dacbed394fe0d0f93dde89ebbc0c4d
ecourse <smkang@codenuri.co.kr>
2025-09-23 11:17:52 +0900

    Add files via upload

--- DAY2/6_생성자1.cpp
+++ DAY2/6_생성자1.cpp
@@ -13,6 +13,16 @@ public:
 		name = n;
 		age = a;
 	}
+		
+	// 생성자(constructor)
+	// 1. class 이름과 동일한 이름의 함수
+	// 2. 반환 타입을 표기하지 않고, 반환 값도 없다.
+	// 3. 객체를 만들면 자동으로 생성자가 호출된다.
+	Person(const std::string& n, int a)
+	{
+		name = n;
+		age = a;
+	}
 };
 
 int main()
@@ -23,8 +33,11 @@ int main()
 //	Person p = { "kim", 28 };
 
 	// #2. 멤버 함수를 사용해서 초기화
-	Person p;
-	p.init("kim", 28);
+//	Person p;
+//	p.init("kim", 28);
+
+	// #3. 생성자를 사용한 초기화
+	Person p("kim", 28); // ok. 이순간 자동으로 생성자 호출
 
 }
 
