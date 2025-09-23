329468d8029bca54e56b84af7d378db11a21c55a
ecourse <smkang@codenuri.co.kr>
2025-09-23 10:24:01 +0900

    Add files via upload

--- DAY2/4_OOP3.cpp
+++ DAY2/4_OOP3.cpp
@@ -6,6 +6,16 @@
 // C   struct  : 함수를 넣을수 없다.
 // C++ struct  : 함수를 넣을수 있다. 
 
+
+// 함수를 구조체 안에 넣어서 얻는 장점은 뭡니까 ?
+// 1. 함수 인자로 데이타를 받을필요 없으므로 간결해 진다
+// 2. 사용하는 코드도 "함수중심" 이 아닌 "데이타(변수, 객체)"중심
+
+// 3. 다양한 객체지향 문법을 사용해서 "안전한 타입(Rect)" 를 만들수 있다
+//    => 앞으로 계속 설명
+
+
+
 struct Rect
 {
 	// 멤버 데이타(사각형의 상태를 표현)
@@ -16,12 +26,12 @@ struct Rect
 
 	// 멤버 함수(사각형 관련 연산제공)
 	// => 멤버함수에서는 멤버 데이타 접근 가능
-	int getRectArea()
+	int getArea()
 	{
 		return (right - left) * (bottom - top);
 	}
 
-	void drawRect()
+	void draw()
 	{
 		std::cout << "draw rect" << std::endl;
 	}
@@ -32,8 +42,8 @@ int main()
 	Rect rc = { 1,1, 10,10 };
 
 	// 객체지향 스타일
-	int n1 = rc.getRectArea();
-	rc.drawRect();
+	int n1 = rc.getArea();
+	rc.draw();
 
 	// C언어 스타일
 //	int n1 = getRectArea(rc);
