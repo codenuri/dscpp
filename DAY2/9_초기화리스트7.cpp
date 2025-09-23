cfced56cc6ab7f1bb0a4180e4e436249d16bee12
ecourse <smkang@codenuri.co.kr>
2025-09-23 15:57:00 +0900

    Add files via upload

--- DAY2/9_초기화리스트7.cpp
+++ DAY2/9_초기화리스트7.cpp
@@ -4,11 +4,11 @@ int g = 10;
 
 class Test
 {
-	int v1 = ++g;
-	int v2 = 0;
+	int v1 = ++g;	// 필드 초기화에는 절대 이런 코드 사용금지
+	int v2 = 0;		// 이처럼 리터럴 정도만 사용하세요. 
 public:
-	Test() {}
-	Test(int a) : v1{ a } {}
+	Test() {}					// Test()      : v1{++g}, v2{0} {}
+	Test(int a) : v1{ a } {}	// Test(int a) : v1{a},   v2{0} {}
 };
 
 int main()
@@ -16,5 +16,5 @@ int main()
 	Test t1;
 	Test t2(3);
 
-	std::cout << g << std::endl; // 얼마 나올지 예측해 보세요
+	std::cout << g << std::endl; // 얼마 나올지 예측해 보세요  11
 }
