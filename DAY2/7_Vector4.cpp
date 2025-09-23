dd6179ba82a0bd31ce88f2314ffac90fec188eb5
ecourse <smkang@codenuri.co.kr>
2025-09-23 13:30:31 +0900

    Add files via upload

---
+++ DAY2/7_Vector4.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+
+// Vector3.cpp 처럼 크기를 변경되는 배열을 "동적 배열" 또는 "Vector" 라고 합니다.
+
+// 객체지향 : 필요한 타입을 먼저 설계 하자.!!
+// => Vector 타입을 설계해 봅시다.
+
+class Vector
+{
+private:
+
+public:
+
+};
+
+int main()
+{
+	Vector v(4); // 크기가 4인 동적 배열
+
+	v.resize(8); // 크기를 8으로!
+}
