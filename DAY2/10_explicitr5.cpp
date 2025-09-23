20f68590d6aac593a7eaaf735532ad009ce0729d
ecourse <smkang@codenuri.co.kr>
2025-09-23 16:52:30 +0900

    Add files via upload

--- DAY2/10_explicitr5.cpp
+++ DAY2/10_explicitr5.cpp
@@ -11,10 +11,21 @@ int main()
 	// 아래 코드의 의미를 생각해 보세요
 	std::vector<int> v = { 1,2,3,4,5 };
 
-	std::cout << v.size() << std::endl; // ????
+	std::cout << v.size() << std::endl; // 5
 
 
 	// 아래 2줄의 차이를 생각해 보세요
-	std::vector<int> v1(10, 0);
-	std::vector<int> v2{10, 0};
+	// =>std::vector<int> 는 () 초기화와 {} 초기화를 다르게 구현
+	// () 초기화 : 갯수, 초기값
+	// {} 초기화 : 요소들.. 
+	std::vector<int> v1(10, 0); // 10개의 요소를 0으로 초기화 해달라.
+	std::vector<int> v2{10, 0}; // 2개를 10, 2로 초기화
+
+
+	std::vector<int> v3(10); // 10개 요소
+	std::vector<int> v4{ 10 }; // 1개 요소를 10으로
+
+	// 아래 코드에서 여러 여부를 생각해보세요
+	std::vector<int> v5 = { 10 };
+	std::vector<int> v6 = 10;
 }
