0fb298a7809cdfd5b60de78e49e3a04277cab392
ecourse <smkang@codenuri.co.kr>
2025-09-23 09:20:11 +0900

    Add files via upload

--- DAY2/1_reference4.cpp
+++ DAY2/1_reference4.cpp
@@ -30,7 +30,7 @@ int main()
 // => 성능 차이는 거의 없음
 // => 단, 안전성을 고려하면 reference 가 좋음.
 void inc1(int* p) { ++(*p); } // good
-void inc2(int& r) { ++r; } // best
+void inc2(int& r) { ++r; }    // best
 
 // 2. in parameter
 // => 인자가 primitive type(int, double) : call by value 가 좋다
@@ -39,3 +39,6 @@ void inc2(int& r) { ++r; } // best
 // => 인자가 user define type 이고 크기가 크다면 (보통 8바이트 이상의크기)
 // => const & 가 좋다.
 // => ex) void foo(const Rect& rc) {}
+
+// 구글에서 "C++ core guideline" 검색
+// 1번째 링크 선택
