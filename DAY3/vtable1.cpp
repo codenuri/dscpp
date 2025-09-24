9d2f4d011117e0e34fea525c986bee663b8cae5a
ecourse <smkang@codenuri.co.kr>
2025-09-24 17:00:35 +0900

    Add files via upload

--- DAY3/vtable1.cpp
+++ DAY3/vtable1.cpp
@@ -1,5 +1,6 @@
 // vtable1.cpp
 // => godbolt.org 에 넣으세요
+#include <iostream>
 
 class Base
 {
@@ -21,5 +22,11 @@ int main()
 	Derived d1;
 	Base* p = &d1;
 	p->f2();	// 이 한줄의 원리는 ?
+
+
+	// b1 : int a 크기 + 테이블 포인터 크기 인때 align 고려
+	// d1 : a, b  크기 + 테이블 포인터 크기 인때 align 고려
+	std::cout << sizeof(b1) << std::endl;
+	std::cout << sizeof(d1) << std::endl;
 }
 // vtable2.cpp 만들어서 그대로 복사해 놓으세요.
