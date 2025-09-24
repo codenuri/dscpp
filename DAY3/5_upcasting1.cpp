cd253f73cf489fa57fc097fbce2e899eeceac73d
ecourse <smkang@codenuri.co.kr>
2025-09-24 15:06:59 +0900

    Add files via upload

---
+++ DAY3/5_upcasting1.cpp
@@ -0,0 +1,29 @@
+﻿// 6_Upcasting1.cpp      140 page ~
+#include <vector>
+
+class Animal
+{
+public:	
+	int age;
+};
+class Dog : public Animal
+{
+public:	
+	int color;
+};
+int main()
+{
+	Dog     dog;
+
+	Dog*    p1 = &dog; // ok
+//	int*    p2 = &dog; // error
+
+	// 핵심 #1. 기반 클래스 포인터로 파생클래스 객체의 주소를 담을수있다
+	// => upcasting
+	// => 가능한 이유는 "메모리 그림"을 생각하세요
+	Animal* p3 = &dog; // ok
+}
+
+
+
+
