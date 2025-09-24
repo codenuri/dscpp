e15b199b006bbb570624ee9d3e9ecf0a153d92ff
ecourse <smkang@codenuri.co.kr>
2025-09-24 10:56:55 +0900

    Add files via upload

--- DAY3/3_static_member1.cpp
+++ DAY3/3_static_member1.cpp
@@ -2,6 +2,12 @@
 #include <iostream>
 
 // Car 객체의 갯수를 관리하고 싶다.
+// => 생성자에서++, 소멸자에서 -- 하면 될것 같다
+
+// 방법 #1. 멤버 데이타 cnt 사용
+// => 실패
+// => 멤버 데이타는 객체별로 따로 생성된다.
+// => 아래 코드는 c1, c2 가 각각 자신의 cnt 사용
 
 class Car
 {
@@ -19,6 +25,6 @@ int main()
 	Car c1;
 	Car c2;
 
-	std::cout << c1.cnt << std::endl;
+	std::cout << c1.cnt << std::endl; // 1
 }
 
