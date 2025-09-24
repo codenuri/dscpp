c1483518987fa94460865f6824442c33c20e66f6
ecourse <smkang@codenuri.co.kr>
2025-09-24 16:34:28 +0900

    Add files via upload

---
+++ DAY3/vtable1.cpp
@@ -0,0 +1,23 @@
+// vtable1.cpp
+
+class Base
+{
+	int a;
+public:
+	virtual void f1() {}
+	virtual void f2() {}
+	virtual void f3() {}
+};
+class Derived : public Base
+{
+public:
+	virtual void f2() {}
+};
+int main()
+{
+	Base b1, b2;
+	Derived d1;
+	Base* p = &d1;
+	p->f2();	// 이 한줄의 원리는 ?
+}
+// vtable2.cpp 만들어서 그대로 복사해 놓으세요.
