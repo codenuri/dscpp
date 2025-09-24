2f4f3e9c1b0c418432eb066e945788ff74db74af
ecourse <smkang@codenuri.co.kr>
2025-09-24 10:54:20 +0900

    Add files via upload

---
+++ DAY3/3_static_member1.cpp
@@ -0,0 +1,24 @@
+﻿// 3_정적멤버1 - 111 page
+#include <iostream>
+
+// Car 객체의 갯수를 관리하고 싶다.
+
+class Car
+{
+	int color;	
+public:
+
+	int cnt = 0;
+
+	Car()  { ++cnt; }
+	~Car() { --cnt; }
+};
+
+int main()
+{
+	Car c1;
+	Car c2;
+
+	std::cout << c1.cnt << std::endl;
+}
+
