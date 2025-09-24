11e2b02f2182e5bdbad2798608f9515035abb2af
ecourse <smkang@codenuri.co.kr>
2025-09-24 09:44:33 +0900

    Add files via upload

--- DAY3/2_const_member_function5.cpp
+++ DAY3/2_const_member_function5.cpp
@@ -20,12 +20,18 @@ public:
 		delete[] ptr;
 	}
 
+	// at() 을 const member function 로 할까요 ?? 하지 말까요 ??
+	// => 아주 어려운 문제.
+	// => 단지, "멤버 함수안에서 상태 변경이 없다.." 정도만이 아닌
+	// => 사용하는 코드를 고려 하세요. 
+	// => 다음 단계부터 해결.. 
 	int& at(int idx)
 	{
 		return ptr[idx];
 	};
-	int size() { return sz; }
-	bool empty() { return sz == 0; }
+
+	int size() const { return sz; }
+	bool empty() const { return sz == 0; }
 
 	void resize(int newsize)
 	{
