b75b2324cb4b7b1f808d42e55e1b51af0bc227ef
ecourse <smkang@codenuri.co.kr>
2025-09-24 13:12:09 +0900

    Add files via upload

--- DAY3/3_static_member7.cpp
+++ DAY3/3_static_member7.cpp
@@ -17,9 +17,16 @@ public:
 		// => 복습할때 해보세요.
 	}
 
+	// getter/setter 제공
+	// => 복습시, year, day 도 해보세요
+	// => set 은 유효 범위도 조사해 보세요.
+	int get_month() const { return month; }  // 주의 get 은 상수멤버함수
+	void set_month(int m) { month = m; }
+
 };
 
 int main()
 {
-
+	Date today{ 2025, 9, 24 };
+	int m = today.get_month();
 }
