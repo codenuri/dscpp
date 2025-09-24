864bfbbd10952d6fae0d5983b2818e38af89c45e
ecourse <smkang@codenuri.co.kr>
2025-09-24 10:47:33 +0900

    Add files via upload

---
+++ DAY3/정리.cpp
@@ -0,0 +1,15 @@
+// 정리.cpp  - 2일차정리.cpp 복사
+// 
+// C++ 수업을 통해서 배워야(알아야)하는 것들
+
+// 1. 대부분의 프로그래밍 언어의 기본 요소를 C++은 어떻게 지원하는가 ?
+// => auto, decltype, {} 초기화, constexpr
+// => inline, default parameter, overloading, template, suffix return type
+// => casting, reference 등...
+
+
+// 2. 객체지향 프로그래밍 문법 (class 문법)
+
+// 3. 객체지향 디자인 기술 => "디자인 패턴 과정"에서 자세히
+
+// 4. C++ 표준라이브러리(STL) 사용법
