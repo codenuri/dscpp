38d72344b602666064ad19e0b6333defa5b65557
ecourse <smkang@codenuri.co.kr>
2025-09-24 09:19:10 +0900

    Add files via upload

---
+++ DAY3/2_const_member_function4.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <string>
+
+class Point
+{
+	int x, y;
+
+	std::string cache;
+	bool cache_valid = false;
+
+public:
+	Point(int a, int b) : x{ a }, y{ b } {}
+
+	std::string to_string() const
+	{
+		if (cache_valid == false)
+		{
+			cache = std::to_string(x) + ", " + std::to_string(y);
+			cache_valid = true;
+		}
+		return cache;
+	}
+
+};
+
+int main()
+{
+	const Point pt{ 1, 2 };
+
+	std::cout << pt.to_string() << std::endl;
+	std::cout << pt.to_string() << std::endl;
+}
+
