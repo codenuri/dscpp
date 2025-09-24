4f9e91ed4ea906af739c7b4a1f78e6a12f99004a
ecourse <smkang@codenuri.co.kr>
2025-09-24 11:26:51 +0900

    Add files via upload

--- DAY3/3_static_member4.cpp
+++ DAY3/3_static_member4.cpp
@@ -16,8 +16,9 @@ public:
 	static int get_count() { return cnt; } // static member function
 											// 객체가 없어도 호출가능
 											// "Car::get_count()"로 호출
+									// 객체와 상관없는 함수
+									// const 붙일수 없다.!
 };
-
 int Car::cnt = 0; // private 이라도 초기화는 가능. 
 
 
@@ -25,11 +26,12 @@ int main()
 {
 	// 자동차를 하나도 만들지 않아도, 자동차의 갯수를 구할수 있어야 한다.
 	// => Car 객체가 없어도 Car::cnt 는 메모리에 있다.
-
+	std::cout << Car::get_count() << std::endl;
 	Car c1;
 	Car c2;
 
-	std::cout << c1.get_count() << std::endl;
+	std::cout << Car::get_count() << std::endl; // <= 권장
+	std::cout << c1.get_count() << std::endl;   // <= 이것도 가능
 
 }
 
