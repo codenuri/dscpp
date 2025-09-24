63fa92a203a8d0acefcbfb60d9253885a2febc5d
ecourse <smkang@codenuri.co.kr>
2025-09-24 13:09:20 +0900

    Add files via upload

--- DAY3/3_static_member7.cpp
+++ DAY3/3_static_member7.cpp
@@ -8,6 +8,15 @@ class Date
 	int year;
 	int month;
 	int day;
+public:
+	Date(int year, int month, int day)
+		: year{ year }, month{ month }, day{ day }
+	{
+		// 값의 유효성을 확인 하려면 초기화 리스트가 아닌 
+		// 여기서 코드 작성
+		// => 복습할때 해보세요.
+	}
+
 };
 
 int main()
