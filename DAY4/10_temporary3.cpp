d69648489bf1fe700cd7c897ccbacee168f3e17b
ecourse <smkang@codenuri.co.kr>
2025-09-25 14:28:20 +0900

    Add files via upload

--- DAY4/10_temporary3.cpp
+++ DAY4/10_temporary3.cpp
@@ -9,19 +9,22 @@ public:
 	~Point() { std::cout << "~Point()" << std::endl; }
 };
 //------------------------------------------------
+// 
 // 함수 인자와 복사본 객체
-void f1(Point  pt) {}
-void f2(Point& pt) {}
+/*
+void f1(Point  pt) {} // call by value     : 복사본 생성, 복사본 이름 pt
+void f2(Point& pt) {} // call by reference : 복사본 생성 안됨. 
 
 int main()
 {
 	Point p{ 0, 0 };
+
 	f1(p);
 	f2(p);
 }
+*/
 //----------------------------------------------
-
-
+// 함수 반환값과 복사본 객체(temporary)
 
 Point pt{ 1, 2 };
 
@@ -30,6 +33,9 @@ Point foo()
 	return pt; 
 }
 
-
+int main()
+{
+	foo();
+}
 
 
