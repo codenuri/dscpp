a9cecddecf92bb9f47f1ed3b9f8092ad1ed664ac
ecourse <smkang@codenuri.co.kr>
2025-09-25 11:02:40 +0900

    Add files via upload

---
+++ DAY4/6_menu_example2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <conio.h>
+
+// 객체지향 프로그램의 핵심
+// => 필요한 타입을 먼저 설계 하라
+
+
+int main()
+{
+	MenuItem m1("김밥", 11);
+	MenuItem m2("라면", 12);
+
+	m1.command(); // 메뉴 선택!
+}
