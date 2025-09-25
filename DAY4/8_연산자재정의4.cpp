c3943d831b0ebbe2677a24916b4bb68679d80e8f
ecourse <smkang@codenuri.co.kr>
2025-09-25 15:38:43 +0900

    Add files via upload

---
+++ DAY4/8_연산자재정의4.cpp
@@ -0,0 +1,41 @@
+//  연산자재정의 - 163 page
+#include <iostream>
+
+class Point
+{
+	int x, y;
+public:
+	Point(int x = 0, int y = 0) : x(x), y(y) {}
+
+	void print() const { std::cout << x << ", " << y << std::endl; }
+
+
+	Point operator+(const Point& pt) const
+	{
+		std::cout << "member operator+\n";
+		Point tmp{ x + pt.x, y + pt.y };
+		return tmp;
+	}
+
+	friend Point operator+(const Point& p1, const Point& p2);
+};
+
+Point operator+(const Point& p1, const Point& p2)
+{
+	std::cout << "non-member operator+()\n";
+
+	Point tmp{ p1.x + p2.x, p1.y + p2.y };
+	return tmp;
+}
+
+
+int main()
+{
+	Point p1{ 1, 1 };
+	Point p2{ 2, 2 };
+
+	Point p3 = p1 + p2;
+
+	p3.print();
+}
+
