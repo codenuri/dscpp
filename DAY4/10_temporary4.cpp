32f5bfba1f1a22ff0526d04f6971d7db28808d2c
ecourse <smkang@codenuri.co.kr>
2025-09-25 14:53:00 +0900

    Add files via upload

--- DAY4/10_temporary4.cpp
+++ DAY4/10_temporary4.cpp
@@ -5,11 +5,15 @@
 // => 요즘 정말 많이 유행합니다.
 // => 코드가 쉬워지고 보기 좋습니다.
 
+// #2. 멤버 함수가 *this 를 반환 한다면 
+// => 반드시 반환 타입은 reference 이어야 합니다.
+
 class Counter
 {
 	int cnt = 0;
 public:
-	Counter increment()
+//	Counter  increment()  // 버그.. 내 자신이 아닌 복사본 반환 입니다.
+	Counter& increment()  // <== 이렇게 해야 합니다.
 	{
 		++cnt;
 
@@ -38,4 +42,14 @@ int main()
 }
 
 
+// 임시객체
+// 1. 사용자가 직접 만들수도 있고
+// => 함수 인자로 객체 보낼때 : draw_line(Point{1,1}, Point{2,2});
+
+// 2. 특정 코드에서 생성될수도 있다.
+// => 함수가 사용자 정의 타입의 값 타입으로 반환 할때
+// => Point foo() ;
+
+// 3. 메소드 체이닝등의 기술을 위해 *this 를 반환 한다면
+// => 반드시 referenfce 타입으로 반환
 
