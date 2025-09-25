f63790a5469ddd43c31b6c090e69b323941f8b40
ecourse <smkang@codenuri.co.kr>
2025-09-25 15:29:53 +0900

    Add files via upload

--- DAY4/8_연산자재정의2.cpp
+++ DAY4/8_연산자재정의2.cpp
@@ -8,6 +8,8 @@ public:
 	Point(int x = 0, int y = 0) : x(x), y(y) {}
 
 	void print() const { std::cout << x << ", " << y << std::endl; }
+
+	friend Point operator+(const Point& p1, const Point& p2);
 };
 
 Point operator+(const Point& p1, const Point& p2 )
@@ -26,5 +28,6 @@ int main()
 	Point p3 = p1 + p2;
 	//       = operator+(p1, p2);  
 
+	p3.print();
 }
 
