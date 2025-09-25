72984a4d700b21fcbfabbea7f02af1ad45866219
ecourse <smkang@codenuri.co.kr>
2025-09-25 16:11:35 +0900

    Add files via upload

--- DAY4/9_cout1.cpp
+++ DAY4/9_cout1.cpp
@@ -7,12 +7,17 @@ int main()
 	int    n = 10;
 	double d = 3.4;
 
-	// p1 + p2	=> p1.operator+(p2)
+	// "p1 + p2"          이 코드를 컴파일러가
+	// "p1.operator+(p2)" 이렇게 변경하는 것처럼
 
-//	std::cout << n; // cout.operator<<(n)
-//	std::cout << d;
+	// "std::cout << n"          을 컴파일러는
+	// "std::cout.operator<<(n)" 로 변경합니다.
 
 	std::cout.operator<<(n); // cout << n 과 동일
+
+
+
+	
 	
 }
 
