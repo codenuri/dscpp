5469df1998b420a9f643bec061466654e2ed6619
ecourse <smkang@codenuri.co.kr>
2025-09-25 13:39:22 +0900

    Add files via upload

---
+++ DAY4/정리.cpp
@@ -0,0 +1,33 @@
+// 정리.cpp 
+// 
+// C++ 수업을 통해서 배워야(알아야)하는 것들
+
+// 1. 대부분의 프로그래밍 언어의 기본 요소를 C++은 어떻게 지원하는가 ?
+// => auto, decltype, {} 초기화, constexpr
+// => inline, default parameter, overloading, template, suffix return type
+// => casting, reference 등...
+
+
+// 2. 객체지향 프로그래밍 문법 (class 문법)
+// => 필요한 타입을 먼저 만들면 프로그램이 쉬워진다..
+// => Point, Rect, Person, Vector 등의 타입을 만들자는것
+// => 타입을 만들때 필요한 문법을 알아야 한다.(class 문법)
+
+// => 1. private, public
+//    2. 생성자, 소멸자
+//    3. 멤버 초기화 리스트
+//    4. this 개념
+//    5. const member function 
+//    6. 선언과 구현을 분리하는 방법
+//    7. static member data, static member function 
+//    8. 복사 생성자 - 5일차 
+// => 모두 이해 해도, 클래스 하나 만들기만 가능
+// => Date, Point, Rect, Vector 등의 한개 타입만 만들수 있다!!
+
+// 3. 객체지향 디자인 기술 => "디자인 패턴 과정"에서 자세히
+// => 상속문법
+// => 객체지향 디자인의 개념
+// => 다양한 디자인 방법들.. 
+
+
+// 4. C++ 표준라이브러리(STL) 사용법
