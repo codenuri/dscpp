e4a1f23136a7517822c28a4b837c6ab6073edf7f
ecourse <smkang@codenuri.co.kr>
2025-09-25 16:17:31 +0900

    Add files via upload

--- DAY4/9_cout2.cpp
+++ DAY4/9_cout2.cpp
@@ -1,13 +1,15 @@
 // 6_cout - 187page
 //#include <iostream>
 #include <cstdio>
-
+// [참고] 실제 출력 코드는 printf 가 아닌 각 OS 의 시스템 콜 사용
+// => 수업은 간단히 하기위해 printf 사용
 namespace std
 {
 	class ostream
 	{
 	public:
-		void operator<<(int n) { printf("%d", n); }
+		void operator<<(int n)    { printf("%d", n); }
+		void operator<<(double d) { printf("%f", d); }
 	};
 	ostream cout;  // cout 은 객체(전역변수)
 }
@@ -18,7 +20,7 @@ int main()
 	double d = 3.4;
 
 	std::cout << n; // cout.operator<<(int)
-//	std::cout << d; // cout.operator<<(double)
+	std::cout << d; // cout.operator<<(double)
 }
 
 
