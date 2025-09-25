004421e7491e93669d4362cc7a57e645cc9754ea
ecourse <smkang@codenuri.co.kr>
2025-09-25 16:19:01 +0900

    Add files via upload

--- DAY4/9_cout2.cpp
+++ DAY4/9_cout2.cpp
@@ -10,6 +10,7 @@ namespace std
 	public:
 		void operator<<(int n)    { printf("%d", n); }
 		void operator<<(double d) { printf("%f", d); }
+		void operator<<(const char* s) { printf("%s", s); }
 	};
 	ostream cout;  // cout 은 객체(전역변수)
 }
@@ -18,6 +19,7 @@ int main()
 {
 	int    n = 10;
 	double d = 3.4;
+	std::cout << "ABCD\n"; // cout.operator<<(const char*) 
 
 	std::cout << n; // cout.operator<<(int)
 	std::cout << d; // cout.operator<<(double)
