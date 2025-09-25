872746f01a8b49bc08406cef89a9a04a8af916c7
ecourse <smkang@codenuri.co.kr>
2025-09-25 09:26:37 +0900

    Add files via upload

--- DAY4/3_shape_example3.cpp
+++ DAY4/3_shape_example3.cpp
@@ -37,15 +37,21 @@ int main()
 		else if (cmd == 2) v.push_back(new Circle);
 		else if (cmd == 9)
 		{
-			for (auto s : v)
+			for (auto s : v) // 현재 auto 타입은 Shape*
 			{
-				s->draw();
+				s->draw(); // A. error
 			}
 		}
 	}
 }
 // 에러가 나는 곳을 찾고.. 
 // 에러의 원인을 생각해 보세요
+// => A 부분에서 s의 타입은 Shape* 
+// => (실제 객체는 Rect, Circle 이지만)포인터 타입은 Shape*
+// => 그런데, Shape에는 draw() 가 없다
+// => "기반 클래스 포인터로 파생 클래스 고유 멤버 접근 안됨"
+// 
 // 해결책도 생각해 보세요
+// => 다음소스 부터.. 
 
 
