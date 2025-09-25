1a475e209403ad282d9e3d9d97472fb08165c041
ecourse <smkang@codenuri.co.kr>
2025-09-25 09:51:30 +0900

    Add files via upload

--- DAY4/3_shape_example4.cpp
+++ DAY4/3_shape_example4.cpp
@@ -3,6 +3,21 @@
 
 // 핵심 #4. 해결책 #1. 캐스팅
 
+// OCP 이론
+// 기능 확장에는 열려있고(Open, 새로운 클래스/모듈 이 추가 되어도)
+// 기존 코드는 수정되지 않게(Close, 코드 수정에는 닫혀 있게)
+// 만들라는 이론(Principle )
+
+// => Open Close Principle(개방폐쇄의 법칙)
+// => 객체지향 프로그래밍 만들때 지켜야 하는 5개의 원칙
+//    SRP, OCP, LSP, ISP, DIP => 앞자를 따서 "SOLID" 원칙
+
+// 아래 main 의 캐스팅 코드는 "OCP" 위반. 안좋은 코드
+
+// 당장 구현만 신경쓰지 말고, 유지 보수르를 고려 하라는 것이 
+// => 객체지향의 핵심 
+
+
 class Shape
 {
 	int color = 0;
@@ -20,6 +35,12 @@ class Circle : public Shape
 {
 public:
 	void draw() { std::cout << "draw circle\n"; }
+};
+
+class Triangle : public Shape
+{
+public:
+	void draw() { std::cout << "draw triangle\n"; }
 };
 
 
