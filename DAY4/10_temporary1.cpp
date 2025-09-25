c99b2d7d50f50035dfcbc0c334f6caa94c2fa107
ecourse <smkang@codenuri.co.kr>
2025-09-25 14:11:42 +0900

    Add files via upload

---
+++ DAY4/10_temporary1.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+// 10_temporary1
+
+// 임시객체 개념
+// => 약간 어려운 개념이지만, 연산자재정의등을 이해하기 위해 필요
+
+class Point
+{
+public:
+	int x, y;
+
+	Point(int a, int b) : x{ a }, y{ b } { std::cout << "Point()" << std::endl; }
+
+	~Point()            { std::cout << "~Point()" << std::endl;	}
+};
+
+int main()
+{
+//	Point pt(1, 2); // 이름이 있는 객체(named object)
+					// 이름 : pt
+					// 특징 : 이름이 있으므로 다음 문장에서도 사용가능.
+				    // 수명 : 자신을 선언한 {} 을 벗어날때 파괴
+
+//	Point (1, 2);	// 이름이 없는 객체(unnamed object)
+					// 특징 : 이름이 없어서 다음 문장에서 접근 안됨
+					// 수명 : 자신을 선언한 문장의 끝(;만날때 파괴)
+
+	Point(1, 2), std::cout << "x\n";
+
+	std::cout << "-------" << std::endl;
+}
+
+
+
+
+
