70e11b4d1760b7c24b8be09fd629b8992fc27138
ecourse <smkang@codenuri.co.kr>
2025-09-25 09:11:37 +0900

    Add files via upload

---
+++ DAY4/3_shape_example1.cpp
@@ -0,0 +1,22 @@
+﻿// 예제1     149 page
+#include <iostream>
+#include <vector>
+
+// 파워 포인트 같은프로그램을 객체지향으로 만드는 것을 생각해 봅시다.
+
+// 핵심 #1. 모든 도형을 타입으로 설계하면 편리하다.
+
+class Rect
+{
+public:
+	void draw() { std::cout << "draw rect\n"; }
+};
+
+
+int main()
+{
+	std::vector<Rect*> v;
+}
+
+
+
