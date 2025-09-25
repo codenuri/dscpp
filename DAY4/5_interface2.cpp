0bfb49daf33f17e279992dd97a986ceb60266dee
ecourse <smkang@codenuri.co.kr>
2025-09-25 13:19:33 +0900

    Add files via upload

---
+++ DAY4/5_interface2.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+
+
+
+
+// 카메라나 사람을 먼저 설계하지말고 
+// 규칙(인터페이스)를 먼저 설계 합니다.
+
+// 규칙 : 모든 카메라는 아래 클래스로 부터 파생 되어야 한다.
+
+class ICamera
+{
+public:
+	virtual void take() = 0;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+class Camera
+{
+public:
+	void take() { std::cout << "take picture" << std::endl; }
+};
+
+class HDCamera
+{
+public:
+	void take() { std::cout << "take HD picture" << std::endl; }
+};
+
+class People
+{
+public:
+	void use_camera(Camera* c) { c->take(); }
+	void use_camera(HDCamera* c) { c->take(); }
+};
+
+int main()
+{
+	People p;
+
+	Camera c;
+	p.use_camera(&c);
+
+	HDCamera hc;
+	p.use_camera(&hc); // ?
+
+}
+
+
+
