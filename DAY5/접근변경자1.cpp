3598a7e6173fc4ac4d484f739afb22f88fdaa953
ecourse <smkang@codenuri.co.kr>
2025-09-26 15:31:12 +0900

    Add files via upload

--- DAY5/접근변경자1.cpp
+++ DAY5/접근변경자1.cpp
@@ -6,7 +6,12 @@ public:
 	int a;
 };
 
-class Derived : public Base
+// private 상속
+// => 기반 클래스 멤버를 상속 받을때, 
+// => private 영역으로 상속해 달라. 
+
+//class Derived : public Base	// 접근 권한 동일하게 유지
+class Derived : private Base	// 접근 권한 축소
 {
 };
 
