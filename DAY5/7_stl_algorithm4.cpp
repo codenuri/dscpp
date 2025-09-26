87ba5195c40ae5b9d844b753785f367f4360bd2b
ecourse <smkang@codenuri.co.kr>
2025-09-26 15:04:43 +0900

    Add files via upload

---
+++ DAY5/7_stl_algorithm4.cpp
@@ -0,0 +1,20 @@
+// 6_알고리즘1
+#include <iostream>
+#include <vector>
+#include <list>
+#include <algorithm>
+
+int main()
+{
+	std::vector<int> v = { 1,2,9,4,3,6,7,3,9,10 };
+
+	// 주어진 구간에서 처음 나오는 "3" 을 찾아라
+	auto ret1 = std::find(v.begin(), v.end(), 3);
+
+
+	// 주어진 구간에서 처음 나오는 "3의 배수" 을 찾아라
+	auto ret2 = std::find(v.begin(), v.end(),  ? );
+
+}
+
+
